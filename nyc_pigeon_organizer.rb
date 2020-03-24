82f54a61dfd3e68d8cbeb7838d0ecb7087a17e4a
Junko Tahara <junkotahara911@gmail.com>
2020-03-24 00:39:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pp'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeons = {}
+  data
 end
+
+
+pigeon_data = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
+nyc_pigeon_organizer(pigeon_data)
