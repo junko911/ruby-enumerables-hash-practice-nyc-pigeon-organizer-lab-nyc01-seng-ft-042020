1dbf1dbaed9fa7e241843bf90c1f0462ec26fc12
Junko Tahara <junkotahara911@gmail.com>
2020-03-24 03:18:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,13 +12,16 @@ def nyc_pigeon_organizer(data)
       inner_index = 0
       while inner_index < value.length do
         items = []
-        items << key
-        
-        if pigeon_list[value[inner_index]].nil?
-          pigeon_list[value[inner_index]] = { category => nil }
-        else
-          pigeon_list[value[inner_index]][category] = nil
+        if data[category][key].has_value? (value[inner_index])
+          items << key
+          pp items
         end
+        
+        # if pigeon_list[value[inner_index]].nil?
+        #   pigeon_list[value[inner_index]] = { category => nil }
+        # else
+        #   pigeon_list[value[inner_index]][category] = nil
+        # end
         
         inner_index += 1
       end
