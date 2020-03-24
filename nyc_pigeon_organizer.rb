6b15b5e2f1f4ce9b6265df4cb074cf04adb824ef
Junko Tahara <junkotahara911@gmail.com>
2020-03-24 04:50:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -31,7 +31,7 @@ def nyc_pigeon_organizer(data)
     index += 1
   end
 
-  pigeon_list
+  pp pigeon_list
 end
 
 # pigeon_list = {
@@ -72,23 +72,23 @@ end
 #   }
 # }
 
-# pigeon_data = {
-#         :color => {
-#           :purple => ["Theo", "Peter Jr.", "Lucky"],
-#           :grey => ["Theo", "Peter Jr.", "Ms. K"],
-#           :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-#           :brown => ["Queenie", "Alex"]
-#         },
-#         :gender => {
-#           :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-#           :female => ["Queenie", "Ms. K"]
-#         },
-#         :lives => {
-#           "Subway" => ["Theo", "Queenie"],
-#           "Central Park" => ["Alex", "Ms. K", "Lucky"],
-#           "Library" => ["Peter Jr."],
-#           "City Hall" => ["Andrew"]
-#         }
-#       }
+pigeon_data = {
+        :color => {
+          :purple => ["Theo", "Peter Jr.", "Lucky"],
+          :grey => ["Theo", "Peter Jr.", "Ms. K"],
+          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+          :brown => ["Queenie", "Alex"]
+        },
+        :gender => {
+          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+          :female => ["Queenie", "Ms. K"]
+        },
+        :lives => {
+          "Subway" => ["Theo", "Queenie"],
+          "Central Park" => ["Alex", "Ms. K", "Lucky"],
+          "Library" => ["Peter Jr."],
+          "City Hall" => ["Andrew"]
+        }
+      }
       
-# nyc_pigeon_organizer(pigeon_data)
+nyc_pigeon_organizer(pigeon_data)
