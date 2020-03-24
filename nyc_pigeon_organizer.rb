d6777b9f22dfe8abf7543b24c7fd97a7b3a39a9e
Junko Tahara <junkotahara911@gmail.com>
2020-03-24 04:28:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,7 +14,8 @@ def nyc_pigeon_organizer(data)
         
         if pigeon_list[value[inner_index]].nil?
           pigeon_list[value[inner_index]] = { category => [key.to_s] }
-        else if pigeon_list[value[inner_index]][category].has_value?(key.to_s] 
+        else if pigeon_list[value[inner_index]][category].include?(key.to_s)
+          true
         end
         
         inner_index += 1
