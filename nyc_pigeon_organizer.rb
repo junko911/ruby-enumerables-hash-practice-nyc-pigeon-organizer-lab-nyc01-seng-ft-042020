1603ba500c0d986cbdf9378f9945107385cf27e2
Junko Tahara <junkotahara911@gmail.com>
2020-03-24 02:26:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,12 +10,12 @@ def nyc_pigeon_organizer(data)
 
     data[category].each do |key, value|
       
-      if pigeon_list[value]
+      if pigeon_list[value.join()].nil?
+        pigeon_list[value.join()] = {}
+      else
+        pigeon_list[value.join()] = { category => key.to_s }
+      end
         
-      
-      [value.join()] = { category => key.to_s }
-      # pp memo
-      memo
     end
     
     index += 1
@@ -24,6 +24,43 @@ def nyc_pigeon_organizer(data)
   pp pigeon_list
 end
 
+# pigeon_list = {
+#   "Theo" => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Subway"]
+#   },
+#   "Peter Jr." => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Library"]
+#   },
+#   "Lucky" => {
+#     :color => ["purple"],
+#     :gender => ["male"],
+#     :lives => ["Central Park"]
+#   },
+#   "Ms. K" => {
+#     :color => ["grey", "white"],
+#     :gender => ["female"],
+#     :lives => ["Central Park"]
+#   },
+#   "Queenie" => {
+#     :color => ["white", "brown"],
+#     :gender => ["female"],
+#     :lives => ["Subway"]
+#   },
+#   "Andrew" => {
+#     :color => ["white"],
+#     :gender => ["male"],
+#     :lives => ["City Hall"]
+#   },
+#   "Alex" => {
+#     :color => ["white", "brown"],
+#     :gender => ["male"],
+#     :lives => ["Central Park"]
+#   }
+# }
 
 pigeon_data = {
   :color => {
