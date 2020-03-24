c871e4e104c11f0e05e927a72da740d6c1ca0fdb
Junko Tahara <junkotahara911@gmail.com>
2020-03-24 02:45:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -67,18 +67,22 @@ end
 #   }
 # }
 
-# pigeon_data = {
-#   :color => {
-#     :brown => ["Luca"],
-#     :black => ["Lola"],
-#   },
-#   :gender => {
-#     :male => ["Luca"],
-#     :female => ["Lola"]
-#   },
-#   :lives => {
-#     "Central Park" => ["Lola"],
-#     "Library" => ["Luca"]
-#   }
-# }
+pigeon_data = {
+        :color => {
+          :purple => ["Theo", "Peter Jr.", "Lucky"],
+          :grey => ["Theo", "Peter Jr.", "Ms. K"],
+          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+          :brown => ["Queenie", "Alex"]
+        },
+        :gender => {
+          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+          :female => ["Queenie", "Ms. K"]
+        },
+        :lives => {
+          "Subway" => ["Theo", "Queenie"],
+          "Central Park" => ["Alex", "Ms. K", "Lucky"],
+          "Library" => ["Peter Jr."],
+          "City Hall" => ["Andrew"]
+        }
+      }
 nyc_pigeon_organizer(pigeon_data)
